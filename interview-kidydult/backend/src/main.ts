b99033daf8b483ef0f90f45a232110f402219b02
Sean Chuah <chuahtseyung2002@gmail.com>
2023-06-26 12:27:39 +0800

    Feat: Using .env for flexible CORS changing

--- interview-kidydult/backend/src/main.ts
+++ interview-kidydult/backend/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+  console.log('Frontend URL for CORS:', process.env.CLIENT_URL);
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:3000'],
+      origin: [process.env.CLIENT_URL],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       credentials: true,
-    }
+    },
   });
   await app.listen(4000);
   console.log('Server running at http://localhost:4000');
